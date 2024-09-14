46996b2d4ad44b1dcb5aab6c52cc7e5c9fc991d2
boujoudev <boujoud31@gmail.com>
2024-09-14 11:17:32 +0100

    adding an easterEgg

--- RPS.js
+++ RPS.js
@@ -1,5 +1,6 @@
 let you=0;
 let cmp=0;
+let drawCount=0;
 
 
 const Scissors=document.querySelector("#scissors-btn")
@@ -8,6 +9,8 @@ const Paper=document.querySelector("#paper-btn")
 const player_score=document.querySelector("#ur_score")
 const cmp_score=document.querySelector("#cmp_score")
 const result=document.querySelector("#result")
+const easterEgg=document.querySelector("#easterEgg")
+const easterEgg_btn=document.querySelector("#easterEgg_btn");
 
 
 const options=["Rock","Paper","Scissors"];
@@ -20,7 +23,7 @@ function win(){
     result.innerText="Congrats you have won +1pts to player"
     you+=1;
     player_score.innerText=you.toString();
-
+    easterEgg.innerHTML=""
 
 
 }
@@ -28,9 +31,20 @@ function loose(){
      result.innerText="Computer won try again +1pts to Computer"
     cmp+=1;
     cmp_score.innerText=cmp.toString();
+    easterEgg.innerHTML=""
 }
 function draw(){
      result.innerText="tie no one get a point "
+    drawCount++;
+    easterEgg.innerHTML=""
+    if (drawCount === 3) {
+        easterEgg.innerHTML = "<button id=\"easterEgg_btn\">Why!!!!</button>";
+        const easterEgg_btn = document.querySelector("#easterEgg_btn");
+        easterEgg_btn.onclick = () => {
+            alert("Easter Egg Activated! Why are you drawing so much?");
+        };
+        drawCount = 0;
+    }
     
 }
 function RPS(str1){
@@ -83,5 +97,6 @@ function RPS(str1){
 Rock.onclick = () => RPS("Rock");
 Paper.onclick = () => RPS("Paper");
 Scissors.onclick = () => RPS("Scissors");
+
 
 
